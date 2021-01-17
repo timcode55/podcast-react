5cd08a7352fd7f5f9d47c891dccabfdbb528acd4
Tim Petrvalsky <tpet55@yahoo.com>
2021-01-17 11:29:27 -0800

    Add hardcoded api data for testing and display is working properly

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import './App.css';
 import CardList from './components/CardList/CardList';
 import Header from './components/Header/Header';
+import listenNotesPodcasts from './listennotes-array';
 
 function App() {
 	const [ state, setState ] = useState({
@@ -13,27 +14,41 @@ function App() {
 	const getPodcasts = () => {
 		axios.get('/podcasts').then((response) => {
 			console.log('button clicked');
+			setState({ podcasts: [ response.data ] });
 			console.log(response.data);
 		});
 	};
 
-	useEffect(() => {
-		async function loadData() {
-			await axios
-				.get('https://jsonplaceholder.typicode.com/users')
-				.then((response) => setState({ podcasts: [ response.data ] }));
-			console.log(state, 'state in useffect');
-		}
-		loadData();
-	}, []);
+	const getTopTwenty = () => {
+		axios.get('/topTwenty').then((response) => {
+			console.log('button clicked');
+			setState({ podcasts: [ response.data ] });
+			console.log(response.data);
+		});
+	};
+
+	const getListenNotes = () => {
+		console.log(listenNotesPodcasts, 'listennotesarray');
+		setState({ podcasts: [ listenNotesPodcasts ] });
+	};
+
+	// useEffect(() => {
+	// 	async function loadData() {
+	// 		await axios.get('/topTwenty').then((response) => setState({ podcasts: [ response.data ] }));
+	// 		console.log(state, 'state in useffect');
+	// 	}
+	// 	loadData();
+	// }, []);
 
 	return (
 		<div>
 			<div className="App">
 				<div className="app-main">
 					<h1>TOP PODCASTS</h1>
-					<Header podcasts={state.podcasts} />
 					<button onClick={getPodcasts}>Get Podcasts</button>
+					<button onClick={getTopTwenty}>Get Top Twenty</button>
+					<button onClick={getListenNotes}>Get Listen Notes</button>
+					<Header podcasts={state.podcasts} />
 				</div>
 				{/* <CardList podcasts={state.podcasts} /> */}
 			</div>
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,56 +1,60 @@
 import React from 'react';
-import './Card2.css';
-// import icons8 from '../../images/Hashtag-26-52px/icons8-hashtag-52.png';
-// import rating from '../../images/Star-24-48px/icons8-star-48.png';
+import './Card.css';
+import icons8 from '../../images/Hashtag-26-52px/icons8-hashtag-52.png';
+import rating from '../../images/Star-24-48px/icons8-star-48.png';
 
-const Card2 = (props) => {
-	console.log(props, 'props in Card2');
+const Card = (props) => {
+	console.log(props, 'props in Card');
 	return (
-		// <div className="podcontainer">
-		<div className="card-container">
-			<img alt="podcast" src={`https://robohash.org/${props.podcast.id}?set=set2&size=180x180`} />
-			<h1>{props.podcast.name}</h1>
-			<p>{props.podcast.email}</p>
+		<div className="podcontainer">
+			<div className="image">
+				<a href={props.podcast.url} target="_blank" rel="noreferrer">
+					<img
+						className="podimage"
+						src={
+							props.podcast.image ? (
+								props.podcast.image
+							) : (
+								'https://upload.wikimedia.org/wikipedia/en/4/4b/The_Joe_Rogan_Experience_logo.jpg'
+							)
+						}
+						alt="pod1"
+					/>
+				</a>
+			</div>
+			<div className="podtitle">
+				<h1>{props.podcast.title.substring(0, 52)}</h1>
+			</div>
+			<div className="desc">
+				<p className="ptext">{props.podcast.title.substring(0, 200).replace(/(<([^>]+)>)/gi, '')}...</p>
+			</div>
+			<div className="podButtons">
+				<div className="webButton">
+					<a href={props.podcast.url} target="_blank" rel="noreferrer">
+						<button>Website</button>
+					</a>
+				</div>
+
+				<div className="webButton">
+					<a href={props.podcast.url} target="_blank" rel="noreferrer">
+						<button>iTunes Link</button>
+					</a>
+				</div>
+			</div>
+			<div className="contratings">
+				<div className="footeritem">
+					<img className="ratingimage" src={icons8} alt="ratingimage" />
+					<p className="ratingtext"># of Ratings</p>
+					<p className="ratingtext">{props.podcast.numberOfRatings}</p>
+				</div>
+				<div className="footeritem">
+					<img className="ratingimage" src={rating} alt="ratingimage" />
+					<p className="ratingtext">iTunes Rating</p>
+					<p className="ratingtext">{props.podcast.rating}</p>
+				</div>
+			</div>
 		</div>
-		// </div>
 	);
 };
 
-export default Card2;
-
-// {
-/* <div className="image">
-				<img className="podimage" src={props.item.image} alt="pod1" />
-				{/* </a> */
-// }
-// </div>
-// <div className="podtitle">
-//   <h1>{props.item.title}</h1>
-// </div>
-// <div className="desc">
-//   <p className="ptext" />
-// </div>
-// <div className="podButtons">
-//   <div className="webButton">
-//     <a href={props.item.website} target="_blank" rel="noreferrer">
-//       <button>Website</button>
-//     </a>
-//   </div>
-//   <div className="webButton">
-//     {/* <a href="#" target="_blank"> */}
-//     <button>iTunes Link</button>
-//     {/* </a> */}
-//   </div>
-// </div>
-// <div className="contratings">
-//   <div className="footeritem">
-//     <img className="ratingimage" src={icons8} alt="ratingimage" />
-//     <p className="ratingtext"># of Ratings</p>
-//     <p className="ratingtext">{props.item.numberRatings}</p>
-//   </div>
-//   <div className="footeritem">
-//     <img className="ratingimage" src={rating} alt="ratingimage" />
-//     <p className="ratingtext">iTunes Rating</p>
-//     <p className="ratingtext">{props.item.rating}</p>
-//   </div>
-// </div> */}
+export default Card;
